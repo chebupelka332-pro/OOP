204c4a296fdfecc4735cb25f27c3d9bada313109
chebupelka332-pro <chebupelka332-pro@users.noreply.github.com>
2025-11-14 15:53:11 +0000

    Deploying to gh-pages from @ chebupelka332-pro/OOP@c9b79d33baeb8a952e00826b221bb07e2a702fe6 🚀

---
+++ Task_1_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.tokarev","l":"Main"},{"p":"ru.nsu.tokarev.SubstringFinder","l":"SubstringFinder"}];updateSearchResults();
