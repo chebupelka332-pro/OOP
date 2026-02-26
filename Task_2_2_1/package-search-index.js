8e4b5d74f37d215767566e115f3d4804ddd1205b
chebupelka332-pro <chebupelka332-pro@users.noreply.github.com>
2026-02-26 16:16:38 +0000

    Deploying to gh-pages from @ chebupelka332-pro/OOP@ad4f7abd034cf5ca44ebad2d6733b1229daf091f 🚀

--- Task_2_2_1/package-search-index.js
+++ Task_2_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.tokarev"},{"l":"ru.nsu.tokarev.Pizzeria"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.tokarev"},{"l":"ru.nsu.tokarev.Pizzeria"},{"l":"ru.nsu.tokarev.Pizzeria.config"},{"l":"ru.nsu.tokarev.Pizzeria.queue"},{"l":"ru.nsu.tokarev.Pizzeria.warehouse"},{"l":"ru.nsu.tokarev.Pizzeria.worker"}];updateSearchResults();
